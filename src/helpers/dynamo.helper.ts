78446eb107663b4697d63b4b31751a62fe0ed0c5
Jean Paul Perea <jean.perea@rappi.com>
2025-02-11 23:28:24 -0500

    Se implementa mapper para facilitar update por transacción

--- src/helpers/dynamo.helper.ts
+++ src/helpers/dynamo.helper.ts
@@ -1,5 +1,7 @@
-import { unmarshall } from "@aws-sdk/util-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { plainToInstance } from "class-transformer";
+import { DynamoWriteItemRequest } from "../dto/request/dynamo-write-item.request";
+import { DynamoWriteOperationType } from "../enums/enums";
 
 export function construct<T>() {
     type Constructor = new () => T;
@@ -22,4 +24,25 @@ export function unmarshallAll(items: Record<string, any>[]) {
 export function unmarshallAndConvert<T>(item: Record<string, any>): T {
     var unmarshalled = unmarshall(item);
     return plainToInstance(construct<T>(), unmarshalled);
+}
+
+export function mapTransactionUpdateItem(table: string, key: { [key: string]: any }, values: { [key: string]: any }): DynamoWriteItemRequest {
+
+    var updateExpression = [];
+    var expressionAttributeNames: { [key: string]: string } = {};
+    var expressionAttributeValues: { [key: string]: any } = {};
+    for (let [key, value] of Object.entries(values)) {
+        updateExpression.push('#' + key + ' = :' + key);
+        expressionAttributeNames['#' + key] = key;
+        expressionAttributeValues[':' + key] = value;
+    }
+
+    return {
+        type: DynamoWriteOperationType.UPDATE,
+        table: table,
+        key: key,
+        update_expression: 'SET ' + updateExpression.join(', '),
+        expression_attribute_names: expressionAttributeNames,
+        expression_attribute_values: expressionAttributeValues,
+    }
 }
